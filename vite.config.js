8666effc9ca810af7b51dd1f67f9af91cf868a0d
ajebt0 <birtr1923@gmail.com>
2026-06-25 09:15:30 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    server: {
-        port: 5173,
-        open: true
-    },
-    optimizeDeps: {
-        include: ['three']
+  server: {
+    port: 5173,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false,
+    minify: 'terser'
+  },
+  base: './', // Важно! Правильные пути для продакшена
+  optimizeDeps: {
+    include: ['three']
+  },
+  resolve: {
+    alias: {
+      '@': '/client'
     }
+  }
 })
